241e22994a0519848f5ba44bfa38daa20b4e8d32
David Wachlin <davidwachlin@gmail.com>
2019-08-13 17:30:40 -0400

    wrote audio feature contollers

--- controllers/audiofeature.js
+++ controllers/audiofeature.js
@@ -1,47 +1,48 @@
 const express = require('express')
 
-/* Step 2
- *
- * Import the api files from the models
- *
- * TODO: change the file path to the models file you'll need to use.
- * TODO: rename this from `templateApi` to something more sensible (e.g:
- * `shopsAPI`)
- *
- * NOTE: You may need to import more than one API to create the 
- * controller you need.
- * 
- */
-const templateApi = require('../models/template.js')
 
-/* Step 3 
- * 
- * Create a new router.
- *
- * the router will "contain" all the request handlers that you define in this file.
- * TODO: rename this from templateRouter to something that makes sense. (e.g:
- * `shopRouter`)
- */
-const templateRouter = express.Router()
+const audioFeatureApi = require('../models/AudioFeature.js')
 
-/* Step 4
- * 
- * TODO: Put all request handlers here
- */
 
-/* Step 5
- *
- * TODO: delete this handler; it's just a sample
- */ 
-templateRouter.get('/', (req, res) => {
-  res.json(templateApi.getHelloWorldString())
+const audioFeatureRouter = express.Router()
+
+audioFeatureRouter.get('/', (req, res) => {
+  const trackId = req.params.trackId
+  audioFeatureApi.getAudioFeatureByTrackId(trackId)
+    .then((audioFeature) => {
+      res.json(audioFeature)
+    })
 })
 
-/* Step 6
- *
- * Export the router from the file.
- *
- */
+audioFeatureRouter.put('/:audioFeatureId', (req, res) => {
+  audioFeatureApi.updateAudioFeature(req.params.audioFeatureId, req.body)
+    .then((updatedAudioFeature) => {
+      res.json(updatedAudioFeature)
+    })
+})
+
+audioFeatureRouter.get('/:audioFeatureId', (req, res) => {
+  audioFeatureApi.getAudioFeature(req.params.audioFeatureId)
+    .then((audioFeature) => {
+      res.json(audioFeature)
+    })
+})
+
+audioFeatureRouter.post('/', (req, res) => {
+  audioFeatureApi.addNewAudioFeature(req.body)
+    .then((audioFeature) => {
+      res.json(audioFeature)
+    })
+})
+
+audioFeatureRouter.delete(':/audioFeatureId', (req, res) => {
+  audioFeatureApi.deleteAudioFeature(req.params.audioFeatureId)
+    .then((audioFeature) => {
+      res.json(audioFeature)
+    })
+})
+
+
 module.exports = {
-  templateRouter
+  audioFeatureRouter
 }
