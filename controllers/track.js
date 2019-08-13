c10dd0a149382c6a8de233009afa3eaad89fa1ea
David Wachlin <davidwachlin@gmail.com>
2019-08-13 17:21:38 -0400

    built track controllers

--- controllers/track.js
+++ controllers/track.js
@@ -1,50 +1,51 @@
-/* Step 1 import express
- *
- */
 const express = require('express')
 
-/* Step 2
- *
- * Import the api files from the models
- *
- * TODO: change the file path to the models file you'll need to use.
- * TODO: rename this from `templateApi` to something more sensible (e.g:
- * `shopsAPI`)
- *
- * NOTE: You may need to import more than one API to create the 
- * controller you need.
- * 
- */
-const templateApi = require('../models/template.js')
+const trackApi = require('../models/track.js')
 
-/* Step 3 
- * 
- * Create a new router.
- *
- * the router will "contain" all the request handlers that you define in this file.
- * TODO: rename this from templateRouter to something that makes sense. (e.g:
- * `shopRouter`)
- */
-const templateRouter = express.Router()
+const trackRouter = express.Router()
 
 /* Step 4
  * 
  * TODO: Put all request handlers here
  */
 
-/* Step 5
- *
- * TODO: delete this handler; it's just a sample
- */ 
-templateRouter.get('/', (req, res) => {
-  res.json(templateApi.getHelloWorldString())
+
+trackRouter.get('/', (req, res) => {
+  trackApi.getAllTracks()
+    .then((tracks) => {
+      res.json(tracks)
+    })
 })
 
-/* Step 6
- *
- * Export the router from the file.
- *
- */
+trackRouter.put('/:trackId', (req, res) => {
+  trackApi.updateTrack(req.params.trackId, req.body)
+    .then((updatedTrack) => {
+      res.json(updatedTrack)
+    })
+})
+
+trackRouter.get('/:trackId', (req, res) => {
+  trackApi.getTrack(req.params.trackId)
+    .then((track) => {
+      res.json(track)
+    })
+})
+
+trackRouter.post('/', (req, res) => {
+  trackApi.addNewTrack(req.body)
+    .then((track) => {
+      res.json(track)
+    })
+})
+
+trackRouter.delete(':/trackId', (req, res) => {
+  trackApi.deleteTrack(req.params.trackId)
+    .then((track) => {
+      res.json(track)
+    })
+})
+
+
 module.exports = {
-  templateRouter
+  trackRouter
 }
