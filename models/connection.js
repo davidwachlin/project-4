a92797548770799293405930020dfe9cba503959
David Wachlin <davidwachlin@gmail.com>
2019-08-20 21:35:20 -0400

    updated connection

--- models/connection.js
+++ models/connection.js
@@ -1,15 +1,21 @@
 
 const mongoose = require('mongoose');
 
+if (process.env.MONGODB_URI) {
+  mongoose.connect(process.env.MONGODB_URI);
+}
+else {
+  mongoose.connect("mongodb://localhost/spotifyproject")
+}
 
-const connectionString = process.env.MONGODB_URI || "mongodb://localhost/spotifyproject";
-
-
-
-mongoose.connect(connectionString, { useNewUrlParser: true})
-  .then(() => {
-    console.log("connected to mongo at: " + connectionString);
-  });
+mongoose.connection.on('error', function(err) {
+  console.error('MongoDB connection error: ' + err);
+  process.exit(-1);
+  }
+);
+mongoose.connection.once('open', function() {
+  console.log("Mongoose has connected to MongoDB!");
+});
 
 
 module.exports = mongoose
